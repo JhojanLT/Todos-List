bc5e46bab2257803172af75dcefcf1b7f88d3790
Jhojan Infante <Jhojaninfante@gmail.com>
2023-06-25 13:54:58 -0500

    First use estated added

--- src/componets/TodoSearch/TodoSearch.js
+++ src/componets/TodoSearch/TodoSearch.js
@@ -2,8 +2,17 @@ import React from "react";
 import "./TodoSearch.scss";
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState("");
   return (
-    <input className="todoSearch" type="text" placeholder="Busca una tarea" />
+    <input
+      className="todoSearch"
+      type="text"
+      placeholder="Busca una tarea"
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+      }}
+    />
   );
 }
 
