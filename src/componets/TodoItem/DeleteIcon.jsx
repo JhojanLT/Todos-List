ae416db7a9f43836311fb4f4fe5af25d6378b0a8
Jhojan Infante <Jhojaninfante@gmail.com>
2023-07-02 23:57:29 -0500

    Nueva rama con versión nueva de iconos agregada (sigue en proceso)

---
+++ src/componets/TodoItem/DeleteIcon.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Trash } from "../../assets/hover=false.svg";
+
+function DeleteIcon() {
+  return "X";
+}
+
+export { DeleteIcon };
--- src/componets/TodoItem/TodoItem.jsx
+++ src/componets/TodoItem/TodoItem.jsx
@@ -1,5 +1,7 @@
 import "./TodoItem.scss";
-import trash from "../../assets/hover=false.svg";
+import { CompleteIcon } from "./CompleteIcon";
+import { DeleteIcon } from "./DeleteIcon";
+// import trash from "../../assets/hover=false.svg";
 function TodoItem({ tarea, status, onComplete, onDelete }) {
   return (
     <li
@@ -7,15 +9,17 @@ function TodoItem({ tarea, status, onComplete, onDelete }) {
         status ? "containerTodoItem--active" : "containerTodoItem"
       }`}
     >
-      <div className="containerTodoItem__mark" onClick={onComplete}></div>
+      <CompleteIcon />
+      {/* <div className="containerTodoItem__mark" onClick={onComplete}></div> */}
       <p className="containerTodoItem__text">{tarea}</p>
       <p className="containerTodoItem__text">{status}</p>
-      <img
+      <DeleteIcon />
+      {/* <img
         className="containerTodoItem__deleteButton"
         src={trash}
         alt="eliminar"
         onClick={onDelete}
-      />
+      /> */}
     </li>
   );
 }
